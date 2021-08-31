e7182ced06cdf46d79ea4c035626334b93e4a33a
Kristen Manning <kristen@brilliant.org>
2021-08-31 15:16:48 -0700

    add tiny API

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const payload = { first_name: "Kristen", dog_name: "Fry" }
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/test-api', (req, res) => res.json(payload))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
